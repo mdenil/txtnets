Fixed cost fuckery and finished with gradients.
I hope.

cpu/model/embedding.py
@@ -19,16 +19,14 @@ class WordEmbedding(layer.Layer):
         self.E = 0.05 * np.random.standard_normal(size=(self.vocabulary_size, self.dimension))
 
     def fprop(self, X, meta):
-        working_space = meta['space_below']
-
-        X, working_space = working_space.transform(X, ['bw', 'd'])
+        X, X_space = meta['space_below'].transform(X, ['bw', 'd'])
 
         Y = self.E[X.ravel()]
 
-        meta['space_above'] = working_space.with_extent(d=self.dimension)
+        meta['space_above'] = X_space.with_extent(d=self.dimension)
         fprop_state = {
-            'input_space': meta['space_below'],
-            'embedding_space': meta['space_above'],
+            'X_space': X_space,
+            'Y_space': meta['space_above'],
             'X': X,
             'Y': Y,
         }
@@ -40,7 +38,7 @@ class WordEmbedding(layer.Layer):
 
         delta_space = meta['space_above']
 
-        delta, delta_space = delta_space.transform(delta, fprop_state['embedding_space'].axes)
+        delta, delta_space = delta_space.transform(delta, fprop_state['Y_space'].axes)
 
         delta = np.sum(Y * delta, axis=1)
         delta_space = delta_space.without_axes('d')
@@ -53,14 +51,14 @@ class WordEmbedding(layer.Layer):
     def grads(self, delta, meta, fprop_state):
         delta_space = meta['space_above']
         X = fprop_state['X']
-        X_space = fprop_state['input_space']
+        X_space = fprop_state['X_space']
 
         delta, delta_space = delta_space.transform(delta, ['bw', 'd'])
         X, X_space = X_space.transform(X, ['bw', 'd'])
 
         grad_E = np.zeros_like(self.E)
-        for i in X.ravel():
-            grad_E[i] += delta[i]
+        for i,j in enumerate(X.ravel()):
+            grad_E[j] += delta[i]
 
         return [grad_E]
 

cpu/model/tests/test_cost.py
@@ -10,18 +10,21 @@ from cpu import space
 class CrossEntropy(unittest.TestCase):
     def setUp(self):
         self.n_classes = 5
-        self.n_data = 50
+        self.n_data = 2
 
         self.cost = model.cost.CrossEntropy()
 
         self.Y = np.random.uniform(size=(self.n_data, self.n_classes))
-        self.Y /= self.Y.sum(axis=1).reshape((-1,1))
+        self.Y /= self.Y.sum(axis=1, keepdims=True)
         self.meta = {
             'space_below': space.Space.infer(self.Y, ['b', 'c'])
         }
 
-        self.Y_true = np.random.randint(0, self.n_classes, size=self.n_data)
+        self.Y_true = np.random.randint(0, self.n_classes-1, size=self.n_data)
         self.Y_true = np.equal.outer(self.Y_true, np.arange(self.n_classes)).astype(self.Y.dtype)
+        # self.Y_true = np.random.uniform(size=(self.n_data, self.n_classes))
+        # self.Y_true /= self.Y_true.sum(axis=1, keepdims=True)
+
 
     def test_fprop(self):
         actual, _, _ = self.cost.fprop(self.Y, self.Y_true, meta=self.meta)
@@ -29,5 +32,36 @@ class CrossEntropy(unittest.TestCase):
 
         assert np.allclose(actual, expected)
 
-    # def test_bprop(self):
-    #     pass
+    def test_bprop(self):
+        def func(y):
+            y = y.reshape((self.n_data, self.n_classes - 1))
+            y = np.hstack([y, 1-np.sum(y, axis=1, keepdims=True)])
+            assert np.allclose(np.sum(y, axis=1),  1)
+            assert np.all(y > 0)
+            assert np.allclose(np.sum(self.Y_true, axis=1), 1)
+            c, meta, cost_state = self.cost.fprop(y, self.Y_true, meta=dict(self.meta))
+            return c
+
+        def grad(y):
+            y = y.reshape((self.n_data, self.n_classes - 1))
+            y = np.hstack([y, 1-np.sum(y, axis=1, keepdims=True)])
+            assert np.allclose(np.sum(y, axis=1),  1)
+            assert np.all(y > 0)
+            assert np.allclose(np.sum(self.Y_true, axis=1), 1)
+            cost, meta, cost_state = self.cost.fprop(y, self.Y_true, meta=dict(self.meta))
+            delta, meta = self.cost.bprop(y, self.Y_true, meta=dict(meta), fprop_state=cost_state)
+            delta = delta[:,:-1]
+            return delta.ravel()
+
+        Y = self.Y[:,:-1]
+        #
+        # print
+        # print func(Y.ravel().copy())
+        # print grad(Y.ravel().copy()).reshape(Y.shape)
+        # print scipy.optimize.approx_fprime(Y.ravel().copy(), func, 1e-8).reshape(Y.shape)
+        #
+        # print scipy.optimize.check_grad(func, grad, Y.ravel().copy())
+        #
+        # assert scipy.optimize.check_grad(func, grad, Y.ravel()) < 1e-5
+
+        self.skipTest('FIXME')

cpu/model/tests/test_embedding.py
@@ -10,7 +10,8 @@ from cpu import space
 class WordEmbedding(unittest.TestCase):
     def setUp(self):
         d = 10
-        vocabulary_size = 10
+        # vocabulary size should be at least as big as the number of words to catch indexing errors
+        vocabulary_size = 30
 
         self.layer = model.embedding.WordEmbedding(
             dimension=d,
@@ -46,4 +47,4 @@ class WordEmbedding(unittest.TestCase):
 
             return grad_E.ravel()
 
-        assert scipy.optimize.check_grad(func, grad, self.layer.E.ravel()) < 1e-5
+        assert scipy.optimize.check_grad(func, grad, self.layer.E.ravel()) < 1e-7

cpu/model/tests/test_transfer.py
@@ -39,16 +39,16 @@ class Softmax(unittest.TestCase):
     def test_bprop(self):
         def func(x):
             x = x.reshape(self.X.shape)
-            Y, meta, fprop_state = self.layer.fprop(x, meta=self.meta)
-            c, meta, cost_state = self.cost.fprop(Y, self.Y, meta=meta)
+            Y, meta, fprop_state = self.layer.fprop(x, meta=dict(self.meta))
+            c, meta, cost_state = self.cost.fprop(Y, self.Y, meta=dict(meta))
             return c
 
         def grad(x):
             X = x.reshape(self.X.shape)
-            Y, meta, fprop_state = self.layer.fprop(X, self.meta)
-            cost, meta, cost_state = self.cost.fprop(Y, self.Y, meta=meta)
-            delta, meta = self.cost.bprop(Y, self.Y, meta=meta, fprop_state=cost_state)
-            delta, _ = self.layer.bprop(delta, meta=meta, fprop_state=fprop_state)
+            Y, meta, fprop_state = self.layer.fprop(X, meta=dict(self.meta))
+            cost, meta, cost_state = self.cost.fprop(Y, self.Y, meta=dict(meta))
+            delta, meta = self.cost.bprop(Y, self.Y, meta=dict(meta), fprop_state=cost_state)
+            delta, _ = self.layer.bprop(delta, meta=dict(meta), fprop_state=fprop_state)
             return delta.ravel()
 
         assert scipy.optimize.check_grad(func, grad, self.X.ravel()) < 1e-5

cpu/model/transfer.py
@@ -18,42 +18,46 @@ class Softmax(layer.Layer):
         self.b = np.zeros(shape=(self.n_classes,1))
 
     def fprop(self, X, meta):
-        working_space = meta['space_below']
-        X, working_space = working_space.transform(X, ['wfd', 'b'])
+        X, X_space = meta['space_below'].transform(X, ['wfd', 'b'])
 
-        fprop_state = {
-            'input_space': meta['space_below'],
-            'X': X,
-        }
+        Y = np.exp(np.dot(self.W, X) + self.b)
+        Y /= np.sum(Y, axis=0)
 
-        X = np.exp(np.dot(self.W, X) + self.b)
-        X /= np.sum(X, axis=0)
-
-        working_space = working_space.without_axes('wf')
-        working_space = working_space.with_extent(d=self.n_classes)
+        Y_space = X_space.without_axes('wf')
+        Y_space = Y_space.with_extent(d=self.n_classes)
 
         meta['lengths'] = np.zeros(meta['lengths'].shape) + self.n_classes
-        meta['space_above'] = working_space
+        meta['space_above'] = Y_space
 
-        return X, meta, fprop_state
+        fprop_state = {
+            'X_space': X_space,
+            'X': X,
+            'Y': Y,
+        }
+
+        return Y, meta, fprop_state
 
     def bprop(self, delta, meta, fprop_state):
-        out = np.dot(self.W.T, delta)
-        meta['space_below'] = fprop_state['input_space']
+        Y = fprop_state['Y']
+
+        out = np.dot(self.W.T, delta * Y * (1-Y))
+
+        meta['space_below'] = fprop_state['X_space']
         return out, meta
 
     def grads(self, delta, meta, fprop_state):
-        # FIXME: This is still not right.  This and cost both compute parts of the derivative but they don't compute
-        # their own parts.
         X = fprop_state['X']
-        X_space = fprop_state['input_space']
+        Y = fprop_state['Y']
+        X_space = fprop_state['X_space']
         X, X_space = X_space.transform(X, ['wfd', 'b'])
 
-        # print meta['space_above'], delta.shape
-        # delta, delta_space = meta['space_above'].transform(delta, ['wfd', 'b'])
+        delta, delta_space = meta['space_above'].transform(delta, ['wfd', 'b'])
+
+        delta = delta * Y * (1-Y)
 
         grad_W = np.dot(delta, X.T)
         grad_b = delta.sum(axis=1).reshape(self.b.shape)
+
         return [grad_W, grad_b]
 
     def params(self):