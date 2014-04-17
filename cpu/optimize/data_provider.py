8d35f63368134d5f65ea878799d2d64a5a8cd27e
Misha Denil <misha.denil@gmail.com>
2014-04-17 16:18:22 +0100

    Add another data provider.
    This file is getting messy.

--- cpu/optimize/data_provider.py
+++ cpu/optimize/data_provider.py
@@ -2,6 +2,7 @@ __author__ = 'mdenil'
 
 import numpy as np
 import random
+from collections import OrderedDict
 
 from cpu import space
 
@@ -105,3 +106,53 @@ class PaddedSequenceMinibatchProvider(object):
 
     def _shuffle_data(self):
         random.shuffle(self.X)
+
+
+class LabelledSequenceMinibatchProvider(object):
+    def __init__(self, X, Y, batch_size, padding, shuffle=True):
+        self.X = X
+        self.Y = Y
+        self.batch_size = batch_size
+        self.padding = padding
+        self.shuffle = shuffle
+
+        self._batch_index = -1
+        self.batches_per_epoch = len(X) / batch_size
+
+    def next_batch(self):
+        self._prepare_for_next_batch()
+
+        batch_start = self._batch_index * self.batch_size
+        batch_end = batch_start + self.batch_size
+
+        X_batch = self.X[batch_start:batch_end]
+        Y_batch = self.Y[batch_start:batch_end]
+
+        Y_batch = np.equal.outer(Y_batch, np.arange(np.max(Y_batch)+1)).astype(np.float)
+
+
+        lengths_batch = np.asarray(map(len, X_batch))
+        max_length_batch = int(lengths_batch.max())
+
+        X_batch = [self._add_padding(x, max_length_batch) for x in X_batch]
+
+        meta = {
+            'lengths': lengths_batch,
+            'space_below': space.Space(axes=['b', 'w'], extent=OrderedDict([('b', len(X_batch)), ('w', max_length_batch)]))
+        }
+
+        return X_batch, Y_batch, meta
+
+    def _prepare_for_next_batch(self):
+        self._batch_index = (self._batch_index + 1) % self.batches_per_epoch
+
+        if self._batch_index == 0 and self.shuffle:
+            self._shuffle_data()
+
+    def _shuffle_data(self):
+        combined = zip(self.X, self.Y)
+        random.shuffle(combined)
+        self.X, self.Y = map(list, zip(*combined))
+
+    def _add_padding(self, x, max_length):
+        return x + [self.padding] * (max_length - len(x))
