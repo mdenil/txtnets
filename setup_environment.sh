9ef85453b29473766336f701dc5a123cece15a21
Misha Denil <misha.denil@gmail.com>
2014-04-21 11:38:25 +0100

    Add sentiment140 data set.

--- setup_environment.sh
+++ setup_environment.sh
@@ -90,6 +90,7 @@ safe_call conda_install psutil
 safe_call conda_install nose
 safe_call conda_install ipython
 safe_call conda_install nltk
+safe_call conda_install pandas
 safe_call pip_install pyprind
 safe_call pip_install pyfftw
 safe_call pip_install --pre line_profiler
