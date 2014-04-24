9542a590c8a4f95f719fb91b700b4da1bedc11ae
Misha Denil <misha.denil@gmail.com>
2014-04-24 10:33:46 +0100

    Remove verify forward pass.
    It's long out of date, and still in the history if I really need it.

--- verify_forward_pass/run_verify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-time matlab -nodisplay -nosplash -nodesktop -r "run('verify_forward_pass/run_matlab_forward_pass.m')"
-time python verify_forward_pass/run_compare_to_matlab.py
