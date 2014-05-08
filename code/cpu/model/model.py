c6d0d97a7c2b849dcd2f9d9166cdd1a05e3dc429
Misha Denil <misha.denil@gmail.com>
2014-05-08 17:51:23 +0100

    Add tagged model collection.

--- code/cpu/model/model.py
+++ code/cpu/model/model.py
@@ -1,6 +1,7 @@
 __author__ = 'mdenil'
 
 import numpy as np
+from collections import OrderedDict
 
 import generic.model.model
 
@@ -11,3 +12,35 @@ import cpu.model.layer
 class CSM(generic.model.model.CSM, cpu.model.layer.Layer):
     Space = cpu.space.CPUSpace
     NDArray = np.ndarray
+
+
+# Models need to have an order in there so grads will work
+class TaggedModelCollection(object):
+    def __init__(self, tagged_models):
+        self.tagged_models = OrderedDict(tagged_models)
+
+    def get_model(self, tag):
+        return self.tagged_models[tag]
+
+    def params(self):
+        params = []
+        for m in self.tagged_models.itervalues():
+            params.extend(m.params())
+        return params
+
+    def full_grads_from_tagged_grads(self, tagged_grads):
+        grads = []
+
+        for tag, model in self.tagged_models.iteritems():
+            if tag in tagged_grads:
+                grads.extend(tagged_grads[tag])
+            else:
+                grads.extend(map(np.zeros_like, model.params()))
+
+        return grads
+
+    def pack(self):
+        packed = []
+        for m in self.tagged_models.itervalues():
+            packed.append(m.pack())
+        return np.concatenate(packed)
