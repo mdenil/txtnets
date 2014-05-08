c9eb50b1894ad9576d23d876a0cbbbf9eb82e832
Misha Denil <misha.denil@gmail.com>
2014-05-08 17:54:06 +0100

    Add data providers for multilingual contrastive embeddings.

--- code/generic/optimize/data_provider.py
+++ code/generic/optimize/data_provider.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 
 import cpu.space
 
+
 class LabelledSequenceMinibatchProvider(object):
     def __init__(self, X, Y, batch_size, padding, shuffle=True, fixed_length=False):
         self.X = X
@@ -71,3 +72,81 @@ class LabelledSequenceBatchProvider(LabelledSequenceMinibatchProvider):
     def __init__(self, X, Y, padding):
         super(LabelledSequenceBatchProvider, self).__init__(
             X, Y, len(X), padding, shuffle=False, fixed_length=False)
+
+
+class SequenceMinibatchProvider(object):
+    def __init__(self, X, batch_size, padding, shuffle=True, fixed_length=False):
+        self.X = X
+        self.batch_size = batch_size
+        self.padding = padding
+        self.fixed_length = fixed_length
+        self.shuffle = shuffle
+
+        self._batch_index = -1
+        self.batches_per_epoch = len(X) / batch_size
+
+    def next_batch(self):
+        self._prepare_for_next_batch()
+
+        batch_start = self._batch_index * self.batch_size
+        batch_end = batch_start + self.batch_size
+
+        X_batch = self.X[batch_start:batch_end]
+
+        lengths_batch = np.asarray(map(len, X_batch))
+
+        if self.fixed_length:
+            max_length_batch = self.fixed_length
+            lengths_batch = np.minimum(lengths_batch, self.fixed_length)
+        else:
+            max_length_batch = int(lengths_batch.max())
+
+        X_batch = [self._pad_or_truncate(x, max_length_batch) for x in X_batch]
+
+        meta = {
+            'lengths': lengths_batch,
+            'space_below': cpu.space.CPUSpace(
+                axes=['b', 'w'],
+                extents=OrderedDict([('b', len(X_batch)), ('w', max_length_batch)]))
+        }
+
+        return X_batch, meta
+
+    def _prepare_for_next_batch(self):
+        self._batch_index = (self._batch_index + 1) % self.batches_per_epoch
+
+        if self._batch_index == 0 and self.shuffle:
+            self._shuffle_data()
+
+    def _shuffle_data(self):
+        random.shuffle(self.X)
+
+    def _pad_or_truncate(self, x, max_length):
+        if max_length > len(x):
+            return x + [self.padding] * (max_length - len(x))
+        else:
+            return x[:max_length]
+
+
+class ParallelProvider(object):
+    def __init__(self, *providers):
+        self.providers = providers
+
+    def next_batch(self):
+        batches = tuple(p.next_batch() for p in self.providers)
+        return batches
+
+
+class TaggedProviderCollection(object):
+    def __init__(self, tagged_providers):
+        self.tagged_providers = tagged_providers
+
+    @property
+    def tags(self):
+        return self.tagged_providers.keys()
+
+    def get_provider(self, tag):
+        return self.tagged_providers[tag]
+
+    def next_batch(self, tag):
+        return self.tagged_providers[tag].next_batch()
