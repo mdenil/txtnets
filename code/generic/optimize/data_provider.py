Fixed bug in DataProvider. Previously if the current batch contained only up to N-1 from N possible labels, the data provider would create the wrong shape Y (batch_size, N-1) instead of (batch_size, N)

code/generic/optimize/data_provider.py
@@ -26,6 +26,7 @@ class LabelledSequenceMinibatchProvider(object):
 
         self._batch_index = -1
         self.batches_per_epoch = len(X) / batch_size
+        self.max_label_value = np.max(Y)
 
     def next_batch(self):
         self._prepare_for_next_batch()
@@ -36,7 +37,7 @@ class LabelledSequenceMinibatchProvider(object):
         X_batch = self.X[batch_start:batch_end]
         Y_batch = self.Y[batch_start:batch_end]
 
-        Y_batch = np.equal.outer(Y_batch, np.arange(np.max(Y_batch)+1)).astype(np.float)
+        Y_batch = np.equal.outer(Y_batch, np.arange(self.max_label_value+1)).astype(np.float)
 
         lengths_batch = np.asarray(map(len, X_batch))
 
@@ -219,6 +220,7 @@ class LabelledDocumentMinibatchProvider(object):
 
         self._batch_index = -1
         self.batches_per_epoch = len(X) / batch_size
+        self.max_label_value = np.max(Y)
 
     def next_batch(self):
         self._prepare_for_next_batch()
@@ -229,7 +231,7 @@ class LabelledDocumentMinibatchProvider(object):
         X_batch = self.X[batch_start:batch_end]
         Y_batch = self.Y[batch_start:batch_end]
 
-        Y_batch = np.equal.outer(Y_batch, np.arange(np.max(Y_batch)+1)).astype(np.float)
+        Y_batch = np.equal.outer(Y_batch, np.arange(self.max_label_value+1)).astype(np.float)
 
         #Just making sure we have the right dimensions
         dimension_b = len(X_batch)