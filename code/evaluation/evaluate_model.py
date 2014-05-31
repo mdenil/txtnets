e9ee5958db1a0479855635da1e67841f44a18a21
Alban Demiraj <aldemiraj@gmail.com>
2014-06-01 00:46:26 +0100

    Add general purpose evaluation module.

--- code/evaluation/evaluate_model.py
+++ code/evaluation/evaluate_model.py
@@ -1,51 +1,28 @@
 __author__ = 'albandemiraj'
 
-
-
-
-__author__ = 'albandemiraj'
-
-
-__author__ = 'albandemiraj'
 import cPickle as pickle
 import os
-import sh
-import ruffus
-import psutil
-from cpu.model.dropout import remove_dropout
 import simplejson as json
 import numpy as np
 from generic.optimize.data_provider import LabelledDocumentMinibatchProvider
-import random
 import time
-import argparse
 
 
+def evaluate_model(model_file, test_file, batch_size, output_dir=None, job_id=None):
+    #CHECKING IF WE ARE WORKING ON A TEST SET
+    assert('test' in test_file)
 
-#progress_dir = "../results"
-output_dir = "../output"
+    #INITIALIZING THE FILENAMES
+    if job_id:
+        output_eval = model_file.rsplit('/')[::-1][0].replace('.pkl', '_'+model_file.rsplit('/')[::-1][1]+'_evaluation.pkl')
+        output_predict = model_file.rsplit('/')[::-1][0].replace('.pkl', '_'+model_file.rsplit('/')[::-1][1]+'_yhat.pkl')
+    else:
+        output_eval = model_file.rsplit('/')[::-1][0].replace('.pkl', '_evaluation.pkl')
+        output_predict = model_file.rsplit('/')[::-1][0].replace('.pkl', '_yhat.pkl')
 
-# trained_models = []
-# model_files = []
-def run():
-    parser = argparse.ArgumentParser(
-    description="Collect cluster data.")
-    parser.add_argument("--model_file", help="where the results subset of folders is")
-    parser.add_argument("--data_dir", default='../data/stanfordmovie')
-    parser.add_argument("--dataset_name", default='stanfordmovie')
-    parser.add_argument("--batch_size", default=5)
 
-    args = parser.parse_args()
-
-    model_file = args.model_file
-    output_eval = model_file.replace('.pkl', '_evaluation.pkl')
-    output_predict = 'yhat_'+output_eval
-    data_dir = args.data_dir
-    dataset_name = args.dataset_name
-    batch_size = args.batch_size
-
-    #PREPARING DIRECTORIES
-    with open(os.path.join(data_dir, dataset_name+".test.sentences.clean.projected.json")) as data_file:
+    #LOADING THE DATA
+    with open(test_file) as data_file:
         data = json.loads(data_file.read())
         X, Y = map(list, zip(*data))
         Y = [[":)", ":("].index(y) for y in Y]
@@ -56,13 +33,13 @@ def run():
         batch_size=batch_size,
         padding='PADDING')
 
-    with open(model_file) as model_file:
-        trained_model = pickle.load(model_file)
+    with open(model_file) as file:
+        trained_model = pickle.load(file)
+
 
     # EVALUATING
     time_start = time.time()
 
-    #EVALUATING
     progress = []
 
     X_valid, full_Y_valid, meta_valid = evaluation_data_provider.next_batch()
@@ -74,6 +51,7 @@ def run():
         Y_hat = trained_model.fprop(X_valid, meta=meta_valid)
         assert np.all(np.abs(Y_hat.sum(axis=1) - 1) < 1e-6)
 
+        #Putting it togather
         full_Y_valid = np.concatenate((full_Y_valid, Y_valid), axis=0)
         full_Y_hat = np.concatenate((full_Y_hat, Y_hat), axis=0)
 
@@ -87,23 +65,35 @@ def run():
 
             progress.append(current)
 
-            with open(output_eval, 'w') as progress_file:
-                pickle.dump(progress, progress_file, protocol=-1)
+            write_data(model_file, output_dir, output_eval, output_predict, full_Y_hat, progress, job_id)
 
-        #FINAL EVALUATION
-        acc = np.mean(np.argmax(full_Y_hat, axis=1) == np.argmax(full_Y_valid, axis=1))
-        current = dict()
-        current['B'] = batch_index
-        current['A'] = acc
-        current['T'] = time.time()-time_start
+    #FINAL EVALUATION
+    acc = np.mean(np.argmax(full_Y_hat, axis=1) == np.argmax(full_Y_valid, axis=1))
+    current = dict()
+    current['B'] = evaluation_data_provider.batches_per_epoch
+    current['A'] = acc
+    current['T'] = time.time()-time_start
 
-        progress.append(current)
+    progress.append(current)
+    write_data(model_file, output_dir, output_eval, output_predict, full_Y_hat, progress, job_id)
 
-        with open(output_eval, 'w') as progress_file:
+def write_data(model_file, output_dir, output_eval, output_predict, full_Y_hat, progress, job_id):
+    if output_dir:
+        with open(os.path.join(output_dir,output_eval), 'w') as progress_file:
             pickle.dump(progress, progress_file, protocol=-1)
 
-        with open(output_predict, 'w') as progress_file:
+        with open(os.path.join(output_dir,output_predict), 'w') as progress_file:
             pickle.dump(full_Y_hat, progress_file, protocol=-1)
+    else:
+        if job_id:
+            file = model_file.replace('.pkl', '_'+model_file.rsplit('/')[::-1][1]+'.pkl')
+        else:
+            file = model_file
 
-if __name__ == "__main__":
-    run()
+        with open(str(file).replace('.pkl', '_evaluation.pkl'), 'w') as progress_file:
+            pickle.dump(progress, progress_file, protocol=-1)
+            progress_file.close()
+
+        with open(str(file).replace('.pkl', '_yhat.pkl'), 'w') as progress_file:
+            pickle.dump(full_Y_hat, progress_file, protocol=-1)
+            progress_file.close()
