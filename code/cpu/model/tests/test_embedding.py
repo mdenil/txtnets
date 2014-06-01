18847e4efbfb90d6e8d55b642068dad9e230e37c
Misha Denil <misha.denil@gmail.com>
2014-06-01 21:51:33 +0100

    Fix embedding test.

--- code/cpu/model/tests/test_embedding.py
+++ code/cpu/model/tests/test_embedding.py
@@ -34,7 +34,7 @@ class WordEmbedding(unittest.TestCase):
 
     def test_fprop(self):
         actual, meta, fprop_state = self.layer.fprop(self.words, meta=self.meta)
-        actual, _ = meta['space_above'].transform(actual, (('b', 'w'), 'd'))
+        actual, _ = meta['space_above'].transform(actual, (('b', 'w'), 'f'))
         expected = self.layer.E[self.words.ravel()]
         assert np.allclose(actual, expected)
 
